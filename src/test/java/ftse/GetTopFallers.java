a24a475d75ca9c87d72350569cbda3f1f8dc66d1
EmreCakmakQA <ECakmak@qa.com>
2021-01-04 17:29:07 +0000

    EC: added FTSE testing for risers and fallers

---
+++ src/test/java/ftse/GetTopFallers.java
@@ -0,0 +1,31 @@
+package ftse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class GetTopFallers {
+	private WebDriver driver;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1366, 768));
+
+    }
+  
+    @Test
+	  public void getTopFallers() {
+	    driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100");
+	    driver.manage().window().setSize(new Dimension(1536, 824));
+	    driver.findElement(By.cssSelector(".one-line:nth-child(3) strong")).click();
+	  }
+  
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+}
