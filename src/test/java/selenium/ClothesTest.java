5a858f9cea173cde33b2233727f32d20ad434505
EmreCakmakQA <ECakmak@qa.com>
2021-01-04 15:31:23 +0000

    EC: updated tests

--- src/test/java/selenium/ClothesTest.java
+++ src/test/java/selenium/ClothesTest.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ClothestestTest {
+public class ClothesTest {
 	 private WebDriver driver;
 
 	    @Before
